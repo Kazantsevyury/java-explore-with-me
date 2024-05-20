16636b8a4073f7c3a2546cb37c0ce40f3834700b
Kazantsev <ka.yu.8@yandex.ru>
2024-05-20 22:49:55 +0200

    feat(stat-client): optimize StatClient module and classes

--- stats/stat-client/src/main/java/ru/practicum/yandex/StatClientImpl.java
+++ stats/stat-client/src/main/java/ru/practicum/yandex/StatClientImpl.java
@@ -12,6 +12,9 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Реализация клиента для взаимодействия с сервисом статистики.
+ */
 @Component
 @Slf4j
 @RequiredArgsConstructor
@@ -19,40 +22,65 @@ public class StatClientImpl implements StatClient {
 
     private final WebClient webClient;
 
+    /**
+     * Отправляет информацию о посещении эндпоинта.
+     *
+     * @param endpointHitDto данные о посещении
+     * @return данные о посещении, сохраненные в сервисе статистики
+     */
     @Override
     public EndpointHitDto methodHit(EndpointHitDto endpointHitDto) {
         String uri = "/hit";
-        log.info("StatClient request on uri '{}'. Body '{}'.", uri, endpointHitDto);
-        return webClient
+        log.info("Запрос StatClient на uri '{}'. Тело '{}'.", uri, endpointHitDto);
+        EndpointHitDto response = webClient
                 .post()
                 .uri(uri)
                 .bodyValue(endpointHitDto)
                 .retrieve()
                 .bodyToMono(EndpointHitDto.class)
                 .block();
+        log.info("Ответ StatClient от uri '{}'. Тело ответа '{}'.", uri, response);
+        return response;
     }
 
+    /**
+     * Получает статистику посещений за указанный период.
+     *
+     * @param start  начало периода
+     * @param end    конец периода
+     * @param uris   список URI для фильтрации
+     * @param unique учитывать только уникальные посещения
+     * @return список статистических данных
+     */
     @Override
     public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
         String requestUrl = String.format("/stats?start=%s&end=%s&uris=%s&unique=%s",
                 URLEncoder.encode(String.valueOf(start), StandardCharsets.UTF_8),
                 URLEncoder.encode(String.valueOf(end), StandardCharsets.UTF_8),
-                uris,
+                String.join(",", uris),
                 unique);
 
-        log.info("StatClient request on uri '{}'.", requestUrl);
-        return webClient.get()
+        log.info("Запрос StatClient на uri '{}'.", requestUrl);
+        List<ViewStatsDto> response = webClient.get()
                 .uri(requestUrl)
                 .retrieve()
                 .bodyToFlux(ViewStatsDto.class)
                 .collectList()
                 .block();
+        log.info("Ответ StatClient от uri '{}'. Тело ответа '{}'.", requestUrl, response);
+        return response;
     }
 
+    /**
+     * Получает уникальную статистику по конкретному URI.
+     *
+     * @param uri URI для получения статистики
+     * @return данные статистики
+     */
     @Override
     public ViewStatsDto getUniqueIpStatsForUri(String uri) {
-        log.info("StatClient request unique stats on uri '{}'.", uri);
-        return webClient.get()
+        log.info("Запрос StatClient на уникальные статистические данные по uri '{}'.", uri);
+        ViewStatsDto response = webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/statistic")
                         .queryParam("uri", uri)
@@ -60,5 +88,7 @@ public class StatClientImpl implements StatClient {
                 .retrieve()
                 .bodyToMono(ViewStatsDto.class)
                 .block();
+        log.info("Ответ StatClient от уникальных статистических данных по uri '{}'. Тело ответа '{}'.", uri, response);
+        return response;
     }
 }
--- stats/stat-client/src/main/java/ru/practicum/yandex/WebConfig.java
+++ stats/stat-client/src/main/java/ru/practicum/yandex/WebConfig.java
@@ -5,17 +5,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
+/**
+ * Конфигурационный класс для настройки WebClient.
+ */
 @Configuration
 public class WebConfig {
 
+    /**
+     * Создает и настраивает экземпляр WebClient.
+     *
+     * @param addressBaseUrl базовый URL сервера статистики, получаемый из настроек приложения
+     * @return настроенный WebClient
+     */
     @Bean
     public WebClient webClient(@Value("${stat-server.url}") String addressBaseUrl) {
-        return WebClient.builder()
-                .baseUrl(addressBaseUrl)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE,
-                        HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+        WebClient webClient = WebClient.create(addressBaseUrl);
+        return webClient.mutate()
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 }
