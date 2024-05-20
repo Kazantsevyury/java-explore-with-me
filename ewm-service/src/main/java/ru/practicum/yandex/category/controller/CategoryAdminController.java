38b3642fab372efd2af7f22c15fca248782cc7d2
Kazantsev <ka.yu.8@yandex.ru>
2024-05-20 22:05:36 +0200

    docs: add documentation for better readability

--- ewm-service/src/main/java/ru/practicum/yandex/category/controller/CategoryAdminController.java
+++ ewm-service/src/main/java/ru/practicum/yandex/category/controller/CategoryAdminController.java
@@ -2,14 +2,8 @@ package ru.practicum.yandex.category.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.yandex.category.dto.CategoryDto;
 import ru.practicum.yandex.category.mapper.CategoryMapper;
 import ru.practicum.yandex.category.model.Category;
@@ -17,11 +11,8 @@ import ru.practicum.yandex.category.service.CategoryService;
 
 import javax.validation.Valid;
 
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.NO_CONTENT;
-
 /**
- * Admin API for categories
+ * API администратора для категорий
  */
 @RestController
 @RequestMapping("/admin/categories")
@@ -30,49 +21,48 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 public class CategoryAdminController {
 
     private final CategoryService categoryService;
-
     private final CategoryMapper categoryMapper;
 
     /**
-     * Add new category. Category name must be unique. If added successfully returns 201 response status.
+     * Добавление новой категории. Имя категории должно быть уникальным. Если добавлено успешно, возвращает статус ответа 201.
      *
-     * @param categoryDto new category parameters
-     * @return added category
+     * @param categoryDto параметры новой категории
+     * @return добавленная категория
      */
     @PostMapping
-    @ResponseStatus(CREATED)
+    @ResponseStatus(HttpStatus.CREATED)
     public CategoryDto addCategory(@RequestBody @Valid CategoryDto categoryDto) {
-        log.info("Adding category '{}'.", categoryDto);
+        log.info("Добавление категории '{}'.", categoryDto);
         final Category category = categoryMapper.toModel(categoryDto);
         final Category addedCategory = categoryService.addCategory(category);
         return categoryMapper.toDto(addedCategory);
     }
 
     /**
-     * Update category. Category name must be unique. If category not found returns 404 response status.
+     * Обновление категории. Имя категории должно быть уникальным. Если категория не найдена, возвращает статус ответа 404.
      *
-     * @param catId             category id to update
-     * @param updateCategoryDto category parameters to update
-     * @return updated category
+     * @param catId             идентификатор категории для обновления
+     * @param updateCategoryDto параметры категории для обновления
+     * @return обновленная категория
      */
     @PatchMapping("/{catId}")
     public CategoryDto updateCategory(@PathVariable Long catId, @RequestBody @Valid CategoryDto updateCategoryDto) {
-        log.info("Updating category with id '{}', new name: '{}'.", catId, updateCategoryDto.getName());
+        log.info("Обновление категории с id '{}', новое имя: '{}'.", catId, updateCategoryDto.getName());
         final Category updateCategory = categoryMapper.toModel(updateCategoryDto);
         final Category updatedCategory = categoryService.updateCategory(catId, updateCategory);
         return categoryMapper.toDto(updatedCategory);
     }
 
     /**
-     * Delete category by category id. Category can not be linked to any events, otherwise returns 409 response status.
-     * If category deleted successfully, returns 204 response status.
+     * Удаление категории по идентификатору категории. Категория не может быть связана с событиями, в противном случае возвращает статус ответа 409.
+     * Если категория успешно удалена, возвращает статус ответа 204.
      *
-     * @param catId category id to delete
+     * @param catId идентификатор категории для удаления
      */
     @DeleteMapping("/{catId}")
-    @ResponseStatus(NO_CONTENT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeCategory(@PathVariable Long catId) {
-        log.info("Deleting category with id '{}'.", catId);
+        log.info("Удаление категории с id '{}'.", catId);
         categoryService.removeCategoryById(catId);
     }
 }
